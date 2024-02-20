87b4bf422a12dbe1ee5d58fcbe46de1d5f7d95d2
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-02-20 20:45:38 +0000

    started to add html to all

---
+++ main.js
@@ -0,0 +1,34 @@
+const positiveQuote = [
+    {
+        quote: `"No one has ever become poor by giving."`,
+        author: `Anne Frank`
+    },
+    {
+        quote: `"Kindness is a language that the deaf hear and the blind see."`,
+        author: `Mark Twain`
+    },
+    {
+        quote: `"Imperfection is beauty, madness is genius, and it's better to be absolutely ridiculous than absolutely boring."`,
+        author: `Marilyn Monroe`
+    },
+    {
+        quote: `"Our greatest glory is not in never falling, but in rising every time we fall."`,
+        author: `Nelson Mandela`
+    },
+    {
+        quote: `"You are loved for who you are, not for what you have."`,
+        author: `Oscar Wilde`
+    },
+    {
+        quote: `"To love yourself is the greatest revolution."`,
+        author: `Nipsey Hussle`
+    },
+    {
+        quote: `"The world is full of magic things, patiently waiting for our senses to grow sharper."`,
+        author: `W.B. Yeats`
+    },
+    {
+        quote: `"We may encounter many defeats but we must not be defeated."`,
+        author: `Maya Angelou`
+    },
+]
